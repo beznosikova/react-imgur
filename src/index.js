6b17d5a0b118383f89421f86a8d04069c3b5a3ec
beznosikova <tanya.beznosikova@avivi.com.ua>
2018-06-02 06:07:14 +0300

    add page galleries list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import App from './App';
+import reducers from "./reducers";
+import GalleriesList from "./components/galleriesList";
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+console.log(store.getState());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router>
+			<Route exact path="/" component={GalleriesList} />
+		</Router>
+	</Provider>, 
+	document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducers/filter.js
@@ -0,0 +1,13 @@
+export default function galleriesFilter(
+  state = {
+    page: "0",
+    section: "hot",
+    sort: "viral",
+    window: "day"
+  }, action) {
+  switch (action.type) {
+    case "CHANGE_FILTER":
+      return Object.assign({}, state, action.payload.params);
+  }
+  return state;
+}
